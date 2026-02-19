212895ce7c71aa6fc4089e991f6d9d1d28f9f0cb
atleer <atle.rimehaug@gmail.com>
2026-02-19 15:53:43 +0100

    add data split to workflow manager

--- run.py
+++ run.py
@@ -4,14 +4,11 @@ from runpy import run_path
 from tqdm import tqdm
 import os
 
-# %%
+# %% Extract relevant time series data from raw data
 
 raw_data_dir = Path('data/raw')
 sel_data_dir = raw_data_dir / 'from_opsd/opsd-time_series-2020-10-06'
-
 filepaths = list(sel_data_dir.glob('**/*60min*.csv'))
-
-# %%
 
 for filepath in tqdm(filepaths, 'Extract time-series data'):
     run_path(
@@ -20,5 +17,19 @@ for filepath in tqdm(filepaths, 'Extract time-series data'):
             'filepath': filepath,
         }
     )
+
+# %% Split extracted time series data into train, validation, and test subsets
+
+processed_data_dir = Path('data/processed')
+filepaths = list(processed_data_dir.glob('**/all_samples/*60*.parquet'))
+
+for filepath in tqdm(filepaths, 'Split into train, val, and test subsets'):
+    run_path(
+        'src/data_pipeline/split_train_test_val.py',
+        init_globals={
+            'filepath': filepath,
+        }
+    )
+
 
 # %%
