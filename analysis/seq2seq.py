d987bae93705a07f3395a460564f1be7464ff573
atleer <atle.rimehaug@gmail.com>
2026-03-16 08:57:02 +0100

    Add script to train seq2seq model to forecast

---
+++ analysis/seq2seq.py
@@ -0,0 +1,203 @@
+#%% Import libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from pathlib import Path
+import pandas as pd
+
+import torch
+import torch.nn as nn
+
+from tqdm import tqdm
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+
+from torch.utils.data import TensorDataset, DataLoader
+
+# %% Check whether google colab kernel is used and download data if it is
+
+import sys 
+
+IN_COLAB = 'google.colab' in sys.modules
+
+if IN_COLAB:
+    # download data
+    import urllib.request
+
+    base_url = "https://raw.githubusercontent.com/atleer/forecast-electricity-markets/main/data/processed/opsd-time_series-2020-10-06"
+    splits = ['train', 'validation', 'test']
+    filename = 'time_series_60min_singleindex.parquet'
+
+    for split in splits:
+        dir_path = f"data/processed/opsd-time_series-2020-10-06/{split}"
+        os.makedirs(dir_path, exist_ok=True)
+        url = f"{base_url}/{split}/{filename}"
+        dest = f"{dir_path}/{filename}"
+        urllib.request.urlretrieve(url, dest)
+        print(f"Downloaded {split}")
+
+    root_dir = Path('.')
+
+# %% Set path to root directory
+
+if IN_COLAB:
+    root_dir = Path('.')
+else:
+    notebook_path = %pwd
+    root_dir = Path(notebook_path).parent
+
+if str(root_dir) not in sys.path:
+    sys.path.insert(0, str(root_dir))
+
+# %% Set seed and turn of non-deterministic behavior for reproducibility
+
+SEED = 2026
+
+def set_seed(seed: int = 2026) -> None:
+    """Set seed and disable non-deterministic behavior for reproducibility"""
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+# %% Set device
+
+def set_device(device: str = 'cpu') -> str:
+    """Set device to CUDA if available. Set to cpu if not."""
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    return device
+
+device = set_device()
+
+# %% Load data
+
+processed_data_dir = root_dir / 'data/processed/opsd-time_series-2020-10-06'
+filepaths = list(processed_data_dir.glob('**/*60*.parquet'))
+
+for filepath in filepaths:
+    if 'train' in filepath.parts:
+        df_train = pd.read_parquet(filepath)
+    elif 'validation' in filepath.parts:
+        df_val = pd.read_parquet(filepath)
+    elif 'test' in filepath.parts:
+        df_test = pd.read_parquet(filepath)
+
+
+# %% Choose columns to use in data
+
+features_column_names = ['DE_wind_generation', 'DE_price_ahead']
+targets_column_names = ['DE_price_ahead']
+
+# %% Scale data
+
+# OPEN QUESTION: Should I use different scaling than standardization with mean?
+
+features_mean = df_train[features_column_names].mean(axis=0).values
+targets_mean = df_train[targets_column_names].mean(axis=0).values
+
+features_std = df_train[features_column_names].std(axis=0).values
+targets_std = df_train[targets_column_names].std(axis=0).values
+
+
+features_train = ((df_train[features_column_names].values - features_mean)/features_std)
+features_val = ((df_val[features_column_names].values - features_mean)/features_std)
+features_test = ((df_test[features_column_names].values - features_mean)/features_std)
+
+targets_train = ((df_train[targets_column_names].values - targets_mean)/targets_std)
+targets_val = ((df_val[targets_column_names].values - targets_mean)/targets_std)
+targets_test = ((df_test[targets_column_names].values - targets_mean)/targets_std)
+
+# %% Create sequences
+
+# TODO: This import won't work on google colab server
+from src.utils import create_sequences
+from models.architectures import Seq2SeqGRU
+
+input_len = 48
+horizon = 24
+
+if targets_train.ndim == 1:
+    targets_train = np.expand_dims(targets_train, axis=-1)
+    targets_val = np.expand_dims(targets_val, axis=-1)
+    targets_test = np.expand_dims(targets_test, axis=-1)
+
+X_train, y_train = create_sequences(features_train, targets_train, input_len, horizon)
+X_val, y_val = create_sequences(features_val, targets_val, input_len, horizon)
+X_test, y_test = create_sequences(features_test, targets_test, input_len, horizon)
+
+X_train = torch.from_numpy(X_train).to(dtype = torch.float32, device=device)
+y_train = torch.from_numpy(y_train).to(dtype = torch.float32, device=device)
+
+X_val = torch.from_numpy(X_val).to(dtype = torch.float32, device=device)
+y_val = torch.from_numpy(y_val).to(dtype = torch.float32, device=device)
+
+X_test = torch.from_numpy(X_test).to(dtype = torch.float32, device=device)
+y_test = torch.from_numpy(y_test).to(dtype = torch.float32, device=device)
+
+# %% Create DataLoaders
+
+train_dataset = TensorDataset(X_train, y_train)
+train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+
+val_dataset = TensorDataset(X_val, y_val)
+val_dataloader = DataLoader(val_dataset, batch_size=len(val_dataset), shuffle=True)
+
+# %% Train
+
+from src.utils import train, train_with_early_stopping
+
+learning_rates = [0.01, 0.001]
+max_epochs = 1
+
+criterion = nn.MSELoss()
+
+model = Seq2SeqGRU(enc_input_size=2, dec_input_size = 1)
+model.eval()
+y_pred_val = model(X_val, horizon = horizon)
+best_loss_val = criterion(y_pred_val, y_val)
+
+for learning_rate in learning_rates:
+
+    model = Seq2SeqGRU(enc_input_size=2, dec_input_size = 1)
+    model.train()
+
+    optimizer = torch.optim.Adam(lr = learning_rate, params=model.parameters())
+
+    losses_train, losses_val, stopped_epoch = train_with_early_stopping(model, 
+                                                    train_dataloader, 
+                                                    val_dataloader,
+                                                    horizon = 24, 
+                                                    optimizer = optimizer, 
+                                                    max_epochs=max_epochs
+                                                )
+
+    model.eval()
+    y_pred_val = model(X_val, horizon = horizon)
+
+    loss_val = criterion(y_pred_val, y_val)
+
+    if  loss_val < best_loss_val:
+        best_loss_val = loss_val
+        best_learning_rate = learning_rate
+        best_hyper_parameters = {
+            'learning_rate': learning_rate
+        }
+
+        torch.save({
+            "stopped_epoch": stopped_epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "loss_validation": best_loss_val,
+        }, 'checkpoint.pth')
+
+
+
+
+
+
+
---
+++ src/utils.py
@@ -0,0 +1,149 @@
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from tqdm import tqdm
+
+
+from torch.utils.data import DataLoader
+
+
+def create_sequences(features, targets, input_len: int = 48, horizon: int = 24):
+    """Create sequences for seq2seq forecasting
+    
+    Args:
+        features: data to use in forecast
+        targets: data to forecast
+        input_len: past timesteps to use in forecast (encoder window length)
+        horizon: future timesteps to forecast (decoder window length)
+
+    Returns:
+        X: (n_samples, input_len, n_features) inputs to encoder
+        y: (n_samples, horizon, n_targets) targets (labels) for decoder
+    """
+
+    n_samples = len(features)
+
+    X = np.empty((n_samples - input_len - horizon, input_len, features.shape[-1]))
+    y = np.empty((n_samples - input_len - horizon, horizon, targets.shape[-1]))
+
+    for i_sample in range(n_samples - input_len - horizon):
+        for i_feature in range(features.shape[-1]):
+            X[i_sample,:,i_feature] = features[i_sample:(i_sample + input_len), i_feature]
+
+        for i_feature in range(targets.shape[-1]):
+            y[i_sample,:,i_feature] = targets[(i_sample + input_len):(i_sample + input_len + horizon), i_feature]
+
+    return np.array(X), np.array(y)
+
+def train(model: nn.Module, dataloader: DataLoader, horizon: int, optimizer: torch.optim.Optimizer, criterion = nn.MSELoss(), nepochs: int = 50):
+    """Train a neural network model
+
+    Args:
+        model: Model to train.
+        dataloader: dataloader containing features and targets
+        horizon: number of time steps into the future to forecast
+        optimizer: optimization algorithm
+        criterion: loss function
+        nepochs: Number of training epochs
+
+    Returns:
+        A tuple of (losses, accuracies). Each is a list of values recorded
+        at each epoch during training.
+    """
+
+
+    losses = []
+    for epoch in tqdm(range(nepochs)):
+        for batch_idx, (X_batch, y_batch) in enumerate(dataloader):
+            optimizer.zero_grad()
+
+            predictions = model(X_batch, horizon=horizon)
+
+            loss = criterion(predictions, y_batch)
+
+            loss.backward()
+
+            optimizer.step()
+
+            losses.append(loss.item())
+
+        if (epoch % 10) == 0:
+            print(f'epoch: {epoch}, loss: {loss.item():.3f}')
+
+    return losses
+
+def train_with_early_stopping(model: nn.Module, 
+                              train_dataloader: DataLoader,
+                              val_dataloader: DataLoader,
+                              horizon: int, 
+                              optimizer: torch.optim.Optimizer, 
+                              criterion = nn.MSELoss(), 
+                              max_epochs: int = 100,
+                              patience: int = 30):
+    """Train a neural network model with early stopping
+
+    Args:
+        model: Model to train.
+        dataloader: dataloader containing features and targets
+        horizon: number of time steps into the future to forecast
+        optimizer: optimization algorithm
+        criterion: loss function
+        max_epochs: Maximal number of training epochs
+        patience: Number of training epochs to wait until you stop training due to no improvement on validation loss
+
+    Returns:
+        A tuple of (losses, accuracies). Each is a list of values recorded
+        at each epoch during training.
+    """
+    losses_val = []
+    for (X_val_batch, y_val_batch) in val_dataloader:
+        y_pred_val = model(X_val_batch, horizon=horizon)
+        losses_val.append(criterion(y_pred_val, y_val_batch).item())
+    best_loss_val = np.mean(losses_val)
+    wait = 0
+    stopped_epoch = max_epochs
+
+    losses_train = []
+    losses_val = []
+    for epoch in tqdm(range(max_epochs)):
+        batch_losses = []
+        for (X_batch, y_batch) in train_dataloader:
+            optimizer.zero_grad()
+
+            predictions = model(X_batch, horizon=horizon)
+
+            loss = criterion(predictions, y_batch)
+
+            loss.backward()
+
+            optimizer.step()
+
+            batch_losses.append(loss.item())
+
+        losses_train.append(np.mean(batch_losses))
+
+        if (epoch % 10) == 0:
+            print(f'epoch: {epoch}, loss: {loss.item():.3f}')
+
+
+        losses_val_temp = []
+        for (X_val_batch, y_val_batch) in val_dataloader:
+            y_pred_val = model(X_val_batch, horizon=horizon)
+            losses_val.append(criterion(y_pred_val, y_val_batch).item())
+        loss_val = np.mean(losses_val_temp)
+        losses_val.append(loss_val)
+
+        if loss_val < best_loss_val:
+            best_loss_val = loss_val
+            wait = 0
+        else:
+            wait += 1
+
+        if wait > patience:
+            stopped_epoch = epoch
+            break
+
+    return losses_train, losses_val, stopped_epoch
