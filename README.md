5a6836062be8bdf7b0b29e19f6dce15bfcfe91d6
atleer <atle.rimehaug@gmail.com>
2026-03-16 08:39:29 +0100

    Add model architectures file

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 ## Forecasting Electricy Markets
 
-### Learning Goals
-- Practice best practices in building a data science pipeline
-- Apply different methods for time series forecasting
-- Get better understanding of power markets
+Apply different methods for time series forecasting.
 
 ### Datasets
 - Downloaded from [Open Power Systems Data](https://open-power-system-data.org/) on 02.11.2025.
---
+++ models/architectures.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+class Seq2SeqGRU(nn.Module):
+    def __init__(self, enc_input_size: int = 2, dec_input_size: int = 1, hidden_size: int = 64, num_layers: int = 1, device: str = 'cpu'):
+        super().__init__()
+        self.encoder = nn.GRU(input_size=enc_input_size, hidden_size=hidden_size, batch_first=True, device=device)
+        self.decoder = nn.GRU(input_size=dec_input_size, hidden_size=hidden_size, batch_first=True, device=device)
+        self.fc = nn.Linear(hidden_size, dec_input_size, device=device)
+
+    def forward(self, X: torch.Tensor, horizon: int, y_teacher: torch.Tensor | None = None, teacher_threshold: float = 0.5):
+
+        # encode input
+        enc_output, hidden = self.encoder(X)
+
+        # seed for decoder: the last observed price
+        dec_input = X[:, -1:, 1:2] # dims: (batch, 1, 1)
+
+        predictions = []
+        for time_step in range(horizon):
+            # decode
+            dec_output, hidden = self.decoder(dec_input, hidden)
+
+            # make prediction from output of decoder
+            prediction = self.fc(dec_output)
+            predictions.append(prediction)
+
+            apply_teacher_forcing = (y_teacher is not None 
+                                     and torch.rand(1).item() < teacher_threshold)
+
+            if apply_teacher_forcing:
+                dec_input = y_teacher[:, time_step:time_step+1].unsqueeze(1)
+            else:
+                dec_input = prediction
+
+
+        # concatinate over dim 1 so that horizon is on second dimension and batches on first
+        return torch.cat(predictions, dim=1)
